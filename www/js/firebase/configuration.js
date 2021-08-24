e7f9446d77a2028c9bb8e2bca525dc2ef2c22e66
GustavoHerreroNunes <ghnunes2010@gmail.com>
2021-08-24 10:55:24 -0300

    Conectando aplicativo ao Firebase

---
+++ www/js/firebase/configuration.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCAkJUyF35oSlFYsFRuVGRGa-F3JzRjuWI",
+    authDomain: "appcrud-firebase-9e94f.firebaseapp.com",
+    projectId: "appcrud-firebase-9e94f",
+    storageBucket: "appcrud-firebase-9e94f.appspot.com",
+    messagingSenderId: "121461857393",
+    appId: "1:121461857393:web:06c044c3908777a9344fd0",
+    measurementId: "G-1TJEM6C0HN"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
