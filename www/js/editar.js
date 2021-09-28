b621ccafca91e33ff4b5fba9cfbbc0b2d790518e
GustavoHerreroNunes <ghnunes2010@gmail.com>
2021-09-28 07:52:37 -0300

    Adicionando método que carrega dados no formulário para a edição

--- www/js/editar.js
+++ www/js/editar.js
@@ -10,87 +10,39 @@ var app = {
     onDeviceReady: function(){
         console.info("Device Ready");
         document.getElementById("btnEditar").addEventListener('click', app.editar);
-        this.receiveEvent('deviceready');
+        app.buscar();
     },
 
-    //Atualiza o DOM ao receber um evento
-    receiveEvent: function(id){
-        console.info("Recebendo Evento");
-        //Abrindo banco de dados, ou criando se não existir
-        db = window.sqlitePlugin.openDatabase({
-            name: 'appCrud.db',
-            location: 'default',
-            androidDatabaseProvider: 'system'
-        });
-
-        //Criando tabela 'clientes' no banco de dados se ela não existir
-        db.transaction( function(tx){
-            tx.executeSql('CREATE TABLE IF NOT EXISTS clients (nome, telefone, origem, data_contato, observacao)');
-        },
-        function(error){
-            console.info('Transaction ERROR: ' + error.message);
-        },
-        function(){
-            console.info("Tabela Criada com sucesso");
-            app.buscar();
-        });
-    },
-
-    //Busca pelo registro selecionado na tabela 'clientes'
+    //Busca os dados do documento a ser editado e os apresenta na tela
     buscar: function(){
-        var url_passada = window.location.href;
-        var url = new URL(url_passada);
-        var telefone_passado = url.searchParams.get("telefone");
+        //Captando parâmetro telefone da url
+        var string_url = window.location.href;
+        var url = new URL(string_url);
+        var doc_telefone = url.searchParams.get("telefone");
 
-        db.executeSql(
-            'SELECT nome AS registNome, telefone AS registTelefone, origem AS registOrigem, data_contato AS registDataContato, observacao AS registObservacao FROM clientes WHERE telefone = ?',
-            [telefone_passado],
-            function(registro){
+        console.log("Telefone: ", doc_telefone);
 
-                let i = 0;
-                for(i = 0; i< registro.rows.length; i++){
-                    document.getElementById("txbNome").value = registro.rows.item(i).registNome;
-                    document.getElementById("txbTelefone").value = registro.rows.item(i).registTelefone;
-                    
-                    document.getElementById("slctOrigem").value = registro.rows.item(i).registOrigem;
+        var db = firebase.firestore();
+        var docSelected = db.collection("cadastros").where("telefone", "==", doc_telefone);
 
-                    document.getElementById("txbDataContato").value = registro.rows.item(i).registDataContato;
-                    
-
-                    document.getElementById("txbObservacao").value = registro.rows.item(i).registObservacao;
-                }
-            },
-            function(error){
-                console.info("Erro ao buscar registro: " + error.message);
-            }
-        );
+        docSelected.get()
+        .then((querySnapshot) =>{
+            querySnapshot.forEach((doc) => {
+                document.getElementById("txbNome").value = doc.data().nome;
+                document.getElementById("txbTelefone").value = doc.data().telefone;
+                document.getElementById("slctOrigem").value = doc.data().origem;
+                document.getElementById("txbDataContato").value = doc.data().data_contato;
+                document.getElementById("txbObservacao").value = doc.data().observacao;
+            });
+        })
+        .catch((error) => {
+            console.info("Erro ao buscar dados do documento: " + error);
+        });
     },
 
-    //Salva as edições feitas em um registro da tabela 'clientes'
-    editar: function(){
-        var url_passada = window.location.href;
-        var url = new URL(url_passada);
-        var telefone_passado = url.searchParams.get("telefone");
+    
+    
 
-        let nome = document.getElementById("txbNome").value;
-        let telefone = document.getElementById("txbTelefone").value;
-        let origem = document.getElementById("slctOrigem").value;
-        let data_contato = document.getElementById("txbDataContato").value;
-        let observacao = document.getElementById("txbObservacao").value;
-
-        db.transaction( function(tx){
-            tx.executeSql(
-                'UPDATE clientes SET nome=?, telefone=?, origem=?, data_contato=?, observacao=? WHERE telefone = ?',
-                [nome, telefone, origem, data_contato, observacao, telefone_passado]
-            );
-        },
-        function(error){
-            console.info("Erro ao salvar alterações: " + error.message);
-        },
-        function(){
-            console.info("Registro editado com sucesso!");
-        });
-    }
 };
 
 app.initialize();
